b3345e65dfc0a69ce655d67222fd76619ce7d9b2
PallaPaIla <mbolduc567@gmail.com>
2025-05-13 10:28:21 -0400

    Put the fake pool in its own file.

--- header/quickhull.h
+++ header/quickhull.h
@@ -9,76 +9,7 @@
 #if __has_include("thread_pool.h")
 #include "thread_pool.h"
 #else
-
-// Create a dummy thread pool rather than changing everything.
-namespace palla {
-    namespace details {
-        namespace quickhull_namespace {
-            class thread_pool {
-            public:
-                // Public types.
-
-                // A dummy sub pool which holds no threads.
-                class sub_pool {
-                    friend class thread_pool;
-                private:
-                    size_t m_desired_size = 0;
-                    sub_pool(size_t desired_size) : m_desired_size(desired_size) {}
-                public:
-                    // sub_pool is default constructible and movable, but not copyable.
-                    sub_pool() = default;
-
-                    void release() { m_desired_size = 0; }
-                    size_t size() const { return 0; }
-                    size_t desired_size() const { return m_desired_size; }
-                    bool empty() const { return size() == 0; }
-                    bool full() const { return size() == desired_size(); }
-
-                    // Dispatchers.
-                    template<class F, class R = std::decay_t<decltype(std::declval<F>()(0))>>
-                    auto dispatch_to_reserved(F&& func) {
-                        if constexpr (!std::is_void_v<R>)
-                            return std::vector<R> {};
-                    }
-                    template<class F, class R = std::decay_t<decltype(std::declval<F>()(0))>>
-                    auto dispatch_to_at_least_one(F&& func) {
-                        if constexpr (std::is_void_v<R>)
-                            func(0);
-                        else
-                            return std::vector<R> { func(0) };
-                    }
-                    template<class F, class R = std::decay_t<decltype(std::declval<F>()(0))>>
-                    auto dispatch_to_all(F&& func) {
-                        if constexpr (std::is_void_v<R>) {
-                            for (int i = 0; i < (int)m_desired_size; i++) {
-                                func(i);
-                            }
-                        }
-                        else {
-                            std::vector<R> results(m_desired_size);
-                            for (int i = 0; i < (int)m_desired_size; i++) {
-                                results[i] = func(i);
-                            }
-                            return results;
-                        }
-                    }
-                };
-
-                // Public functions.
-                static thread_pool get() { return {}; }
-                void disable() {}
-                void enable() {}
-                void resize(size_t) {}
-                size_t size() const { return 0; }
-                bool empty() const { return true; }
-                size_t nb_available() const { return 0; }
-                size_t nb_working() const { return 0; }
-                bool is_worker() const { return false; }
-                sub_pool reserve(size_t nb_desired) { return sub_pool(nb_desired); }
-            };
-        }
-    }
-}
+#include "fake_thread_pool.h"
 #endif
 
 
